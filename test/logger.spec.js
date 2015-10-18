fb85fc566f17e8fd90ee069edc01385ca8b793c1
Alasdair Mercer <mercer.alasdair@gmail.com>
2015-10-18 22:47:06 +0100

    added tests for logger.js and added new result and started methods to Moan API

---
+++ test/logger.spec.js
@@ -0,0 +1,157 @@
+/*
+ * moan
+ * https://github.com/neocotic/moan
+ *
+ * Copyright (c) 2015 Alasdair Mercer
+ * Licensed under the MIT license.
+ * https://github.com/neocotic/moan/blob/master/LICENSE.md
+ */
+
+'use strict'
+
+const chalk = require('chalk')
+const expect = require('expect.js')
+const sinon = require('sinon')
+const Writable = require('stream').Writable
+
+const Logger = require('../lib/logger')
+const Moan = require('../lib/moan').Moan
+
+describe('Logger', () => {
+  let moan
+  let options
+
+  let logger
+
+  /**
+   * TODO: Document
+   *
+   * @param {string} methodName -
+   * @param {Object} expectations -
+   * @param {boolean} expectations.appendNewLine -
+   * @param {string} expectations.prefix -
+   * @param {boolean} expectations.prefixTask -
+   * @param {boolean} expectations.requiresDebug -
+   * @param {string} expectations.stream -
+   * @private
+   */
+  function describeWriteMethod(methodName, streamOption, expectedPrefix, expectTaskName, expectNewLine, requiresDebug) {
+    describe(`#${methodName}`, () => {
+      let stubWrite
+      let prefix = expectedPrefix ? `${expectedPrefix} ` : ''
+      let suffix = expectNewLine ? '\n' : ''
+      let message = `foo ${chalk.cyan('bar')}`
+      let expectedMessage = `${prefix}${message}${suffix}`
+
+      beforeEach(() => {
+        stubWrite = options[streamOption].write
+      })
+
+      it(`should write message to "${streamOption}" stream`, () => {
+        expect(logger[methodName](message)).to.be(logger)
+
+        expect(stubWrite.callCount).to.be(1)
+        expect(stubWrite.args).to.eql([ [ expectedMessage ] ])
+      })
+
+      context('when no message is provided', () => {
+        it(`should write ${expectNewLine ? 'blank line' : 'empty string'}`, () => {
+          expect(logger[methodName]()).to.be(logger)
+
+          expect(stubWrite.callCount).to.be(1)
+          expect(stubWrite.args).to.eql([ [ `${prefix.trim()}${suffix}` ] ])
+        })
+      })
+
+      context('when color mode is disabled', () => {
+        it('should write message without color', () => {
+          moan.color = false
+
+          expect(logger[methodName](message)).to.be(logger)
+
+          expect(stubWrite.callCount).to.be(1)
+          expect(stubWrite.args).to.eql([ [ chalk.stripColor(expectedMessage) ] ])
+        })
+      })
+
+      context('when a task is running', () => {
+        if (expectTaskName) {
+          it('should write message with task name prefix', (done) => {
+            moan.task('default', () => {
+              expect(logger[methodName](message)).to.be(logger)
+
+              expect(stubWrite.callCount).to.be(1)
+              expect(stubWrite.args).to.eql([ [ `${chalk.inverse('[default]')} ${expectedMessage}` ] ])
+            })
+
+            moan.run()
+              .then(() => done())
+              .catch(done)
+          })
+        } else {
+          it('should write message without task name prefix', (done) => {
+            moan.task('default', () => {
+              expect(logger[methodName](message)).to.be(logger)
+
+              expect(stubWrite.callCount).to.be(1)
+              expect(stubWrite.args).to.eql([ [ expectedMessage ] ])
+            })
+
+            moan.run()
+              .then(() => done())
+              .catch(done)
+          })
+        }
+      })
+
+      context('when debug mode is disabled', () => {
+        beforeEach(() => {
+          moan.debug = false
+        })
+
+        if (requiresDebug) {
+          it('should write nothing', () => {
+            expect(logger[methodName](message)).to.be(logger)
+
+            expect(stubWrite.callCount).to.be(0)
+          })
+        } else {
+          it(`should write message to "${streamOption}" stream`, () => {
+            expect(logger[methodName](message)).to.be(logger)
+
+            expect(stubWrite.callCount).to.be(1)
+            expect(stubWrite.args).to.eql([ [ expectedMessage ] ])
+          })
+        }
+      })
+    })
+  }
+
+  beforeEach(() => {
+    let err = new Writable()
+    let out = new Writable()
+
+    sinon.stub(err, 'write')
+    sinon.stub(out, 'write')
+
+    let dummyWritable = {
+      write: () => {}
+    }
+
+    moan = new Moan({ debug: true })
+    moan.log = new Logger(moan, {
+      err: dummyWritable,
+      out: dummyWritable
+    })
+    options = { err, out }
+
+    logger = new Logger(moan, options)
+  })
+
+  describeWriteMethod('debug', 'out', chalk.blue('DEBUG'), true, true, true)
+  describeWriteMethod('error', 'err', chalk.red('ERROR'), true, true)
+  describeWriteMethod('ok', 'out', chalk.green('OK'), true, true)
+  describeWriteMethod('warn', 'out', chalk.yellow('WARNING'), true, true)
+  describeWriteMethod('write', 'out', null, false, false)
+  describeWriteMethod('writeln', 'out', null, true, true)
+})
--- test/utils.spec.js
+++ test/utils.spec.js
@@ -15,44 +15,54 @@ const Utils = require('../lib/utils')
 
 describe('Utils', () => {
   describe('.asArray', () => {
-    it('returns an empty array if null or undefined', () => {
-      expect(Utils.asArray(null)).to.eql([])
-      expect(Utils.asArray(undefined)).to.eql([])
+    context('when argument is null or undefined', () => {
+      it('should return an empty array', () => {
+        expect(Utils.asArray(null)).to.eql([])
+        expect(Utils.asArray(undefined)).to.eql([])
+      })
     })
 
-    it('returns argument if an array', () => {
-      let array = []
+    context('when argument is an array', () => {
+      it('should return argument', () => {
+        let array = []
 
-      expect(Utils.asArray(array)).to.be(array)
+        expect(Utils.asArray(array)).to.be(array)
 
-      array.push('foo', 'bar')
+        array.push('foo', 'bar')
 
-      expect(Utils.asArray(array)).to.be(array)
+        expect(Utils.asArray(array)).to.be(array)
+      })
     })
 
-    it('returns an array containing argument when not an array', () => {
-      expect(Utils.asArray('foo')).to.eql([ 'foo' ])
+    context('when argument is not an array', () => {
+      it('should return an array containing argument', () => {
+        expect(Utils.asArray('foo')).to.eql([ 'foo' ])
+      })
     })
   })
 
   describe('.asString', () => {
-    it('returns an empty string if null or undefined', () => {
-      expect(Utils.asString(null)).to.be('')
-      expect(Utils.asString(undefined)).to.be('')
+    context('when argument is null or undefined', () => {
+      it('should return an empty string', () => {
+        expect(Utils.asString(null)).to.be('')
+        expect(Utils.asString(undefined)).to.be('')
+      })
     })
 
-    it('returns string representation of argument', () => {
+    context('when argument is an array', () => {
+      it('should return a string containing comma-separated values', () => {
+        expect(Utils.asString([])).to.be('')
+        expect(Utils.asString([ 'foo' ])).to.be('foo')
+        expect(Utils.asString([ 'foo', 'bar' ])).to.be('foo, bar')
+      })
+    })
+
+    it('should return a string representation of argument', () => {
       expect(Utils.asString('foo')).to.be('foo')
       expect(Utils.asString(123)).to.be('123')
       expect(Utils.asString(-987)).to.be('-987')
       expect(Utils.asString(NaN)).to.be('NaN')
       expect(Utils.asString(true)).to.be('true')
-    })
-
-    it('returns a list of comma-separated values if argument is an array', () => {
-      expect(Utils.asString([])).to.be('')
-      expect(Utils.asString([ 'foo' ])).to.be('foo')
-      expect(Utils.asString([ 'foo', 'bar' ])).to.be('foo, bar')
     })
   })
 })

5dbbda3069476a27f82db1cd7ac841d110a9a4cf
Alasdair Mercer <mercer.alasdair@gmail.com>
2015-10-20 01:32:27 +0100

    removed unused deps and documented MochaRunner helper

--- task/helper/mocha-runner.js
+++ task/helper/mocha-runner.js
@@ -21,13 +21,23 @@ const mochaSymbol = Symbol('mocha')
 const patternsSymbol = Symbol('patterns')
 const runSymbol = Symbol('run')
 
-// TODO: complete
-
+/**
+ * Runs a mocha test suite for based on a set of file patterns (globs).
+ *
+ * @public
+ */
 module.exports = class MochaRunner {
 
+  /**
+   * Creates a new instance of {@link MochaRunner} for test files identified by the glob <code>patterns</code>
+   * provided.
+   *
+   * @param {string|string[]} patterns - the glob pattern to target the files for the test suite
+   * @param {Object} options - the options to be used
+   * @param {string} options.reporter - the mocha reporter to be used
+   * @public
+   */
   constructor(patterns, options) {
-    options = options || {}
-
     this[mochaSymbol] = new Mocha({
       reporter: options.reporter
     })
@@ -35,6 +45,12 @@ module.exports = class MochaRunner {
     this[patternsSymbol] = Utils.asArray(patterns)
   }
 
+  /**
+   * Loads each of the specified <code>files</code> into this mocha test suite.
+   *
+   * @param {string[]} files - the test files to be loaded into the suite
+   * @private
+   */
   [loadSuiteSymbol](files) {
     for (let file of files) {
       moan.log.debug(`Adding file to test suite: ${path.normalize(file)}`)
@@ -43,6 +59,12 @@ module.exports = class MochaRunner {
     }
   }
 
+  /**
+   * Loads all test files into this suite and then runs them all for the configured reporter.
+   *
+   * @return {Promise} A <code>Promise</code> used to track the test suite execution.
+   * @private
+   */
   run() {
     return globby(this[patternsSymbol])
       .then((files) => {
@@ -52,6 +74,12 @@ module.exports = class MochaRunner {
       })
   }
 
+  /**
+   * Runs the full test suite and reports any failure.
+   *
+   * @private {Promise} A <code>Promise</code> used to track the test suite execution.
+   * @private
+   */
   [runSymbol]() {
     return new Promise((resolve, reject) => {
       let files = this[mochaSymbol].files

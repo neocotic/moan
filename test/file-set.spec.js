5ce24edbed4992a947b7dfe176fabb819e20bba4
Alasdair Mercer <mercer.alasdair@gmail.com>
2015-10-23 17:29:40 +0100

    tidied up tests and made some good progress

--- test/file-set.spec.js
+++ test/file-set.spec.js
@@ -11,19 +11,32 @@
 
 const EventEmitter = require('events').EventEmitter
 const expect = require('expect.js')
-
-const FileSet = require('../lib/file-set')
-const FixtureHelper = require('./helper/fixture-helper')
+const mockery = require('mockery')
+const sinon = require('sinon')
 
 describe('FileSet', () => {
-  let fixtureHelper
+  let FileSet
+  let stubDel
+  let stubGlobby
 
-  before(() => {
-    fixtureHelper = new FixtureHelper()
+  beforeEach(() => {
+    mockery.enable({
+      useCleanCache: true,
+      warnOnReplace: false,
+      warnOnUnregistered: false
+    })
+
+    stubDel = sinon.stub()
+    stubGlobby = sinon.stub()
+
+    mockery.registerMock('del', stubDel)
+    mockery.registerMock('globby', stubGlobby)
+
+    FileSet = require('../lib/file-set')
   })
 
-  after(() => {
-    fixtureHelper.cleanUp()
+  afterEach(() => {
+    mockery.disable()
   })
 
   it('should extend from EventEmitter', () => {
@@ -34,8 +47,8 @@ describe('FileSet', () => {
 
   describe('#constructor', () => {
     it('should setup class correctly', () => {
-      let patterns = [ 'fu', 'baz' ]
-      let options = { nocase: true }
+      let patterns = [ '**/*.md', '**/*.txt' ]
+      let options = { nocase: true, nodir: true }
 
       let fileSet = new FileSet(patterns, options)
 
@@ -52,92 +65,422 @@ describe('FileSet', () => {
 
     context('when string is used for patterns', () => {
       it('should have a single pattern', () => {
-        let options = { nocase: true }
+        let options = { nocase: true, nodir: true }
 
-        let fileSet = new FileSet('bar', options)
+        let fileSet = new FileSet('**/*.md', options)
 
-        expect(fileSet.patterns).to.eql([ 'bar' ])
+        expect(fileSet.patterns).to.eql([ '**/*.md' ])
       })
     })
   })
 
   describe('#del', () => {
-    let temp
+    context('when successful', () => {
+      it('should use "del" module and pass the correct glob patterns and options', (done) => {
+        let expected = [ 'bar.txt', 'baz.md', 'foo.txt', 'fu.md' ]
+        let patterns = [ '**/*.md', '**/*.txt' ]
+        let options = { nocase: true, nodir: true }
+        let fileSet = new FileSet(patterns, options)
 
-    beforeEach((done) => {
-      fixtureHelper.copy()
-        .then((tempDirectory) => {
-          temp = tempDirectory
+        stubDel.withArgs(patterns, options).returns(Promise.resolve(expected))
+
+        fileSet.del()
+          .then((files) => {
+            expect(files).to.eql(expected)
+
+            done()
+          })
+          .catch(done)
+      })
+
+      it('should emit a "deleted" event', (done) => {
+        let expected = [ 'bar.txt', 'baz.md', 'foo.txt', 'fu.md' ]
+        let patterns = [ '**/*.md', '**/*.txt' ]
+        let fileSet = new FileSet(patterns)
+
+        stubDel.withArgs(patterns, {}).returns(Promise.resolve(expected))
+
+        fileSet.on('deleted', (files) => {
+          expect(files).to.eql(expected)
 
           done()
         })
-        .catch(done)
+
+        fileSet.del()
+          .catch(done)
+      })
     })
 
-    afterEach(() => {
-      fixtureHelper.cleanUp(temp)
+    context('when unsuccessful', () => {
+      it('should propagate errors correctly from the "del" module', (done) => {
+        let expected = new Error('foo')
+        let patterns = [ '**/*.md', '**/*.txt' ]
+        let fileSet = new FileSet(patterns)
+
+        stubDel.withArgs(patterns, {}).returns(Promise.reject(expected))
+
+        fileSet.del()
+          .then(() => {
+            expect().fail('Should have been rejected')
+
+            done()
+          })
+          .catch((error) => {
+            expect(error).to.be(expected)
+
+            done()
+          })
+      })
+
+      it('should emit an "error" event', (done) => {
+        let expected = new Error('foo')
+        let patterns = [ '**/*.md', '**/*.txt' ]
+        let fileSet = new FileSet(patterns)
+
+        stubDel.withArgs(patterns, {}).returns(Promise.reject(expected))
+
+        fileSet.on('error', (error) => {
+          expect(error).to.eql(expected)
+
+          done()
+        })
+
+        fileSet.del()
+          .then(() => {
+            expect().fail('Should have been rejected')
+
+            done()
+          })
+      })
+    })
+  })
+
+  describe('#expand', () => {
+    it('should extend FileSet correctly', () => {
+      let patterns = [ '**/*.md', '**/*.txt' ]
+      let options = { nocase: true }
+      let otherPatterns = [ '**/*.1st', '**/*.doc' ]
+      let otherOptions = { dir: true }
+
+      let fileSet = new FileSet(patterns, options)
+      let copy = fileSet.expand(otherPatterns, otherOptions)
+
+      expect(copy.patterns).to.eql(patterns.concat(otherPatterns))
     })
 
-    // TODO: Complete unit tests
+    context('when nothing is provided and original has no patterns', () => {
+      it('should have no patterns', () => {
+        let fileSet = new FileSet()
+        let copy = fileSet.expand()
+
+        expect(copy.patterns).to.eql([])
+      })
+    })
+
+    context('when nothing is provided and original has patterns', () => {
+      it('should have only the patterns of the original', () => {
+        let patterns = [ '**/*.md', '**/*.txt' ]
+        let options = { nocase: true, nodir: true }
+
+        let fileSet = new FileSet(patterns, options)
+        let copy = fileSet.expand()
+
+        expect(copy.patterns).to.eql(patterns)
+      })
+    })
+
+    context('when string is used for patterns and original has no patterns', () => {
+      it('should have a single pattern', () => {
+        let fileSet = new FileSet()
+        let copy = fileSet.expand('**/*.md')
+
+        expect(copy.patterns).to.eql([ '**/*.md' ])
+      })
+    })
+
+    context('when string is used for patterns and origina has patterns', () => {
+      it('should extend the patterns of the original', () => {
+        let patterns = [ '**/*.md', '**/*.txt' ]
+        let options = { nocase: true }
+        let otherPattern = '**/*.1st'
+        let otherOptions = { dir: true }
+
+        let fileSet = new FileSet(patterns, options)
+        let copy = fileSet.expand(otherPattern, otherOptions)
+
+        expect(copy.patterns).to.eql(patterns.concat(otherPattern))
+      })
+    })
   })
 
   describe('#first', () => {
-    let temp
+    context('when successful', () => {
+      it('should use "globby" module and pass the correct glob patterns and options', (done) => {
+        let expected = 'bar.txt'
+        let files = [ expected, 'baz.md', 'foo.txt', 'fu.md' ]
+        let patterns = [ '**/*.md', '**/*.txt' ]
+        let options = { nocase: true, nodir: true }
+        let fileSet = new FileSet(patterns, options)
 
-    before((done) => {
-      fixtureHelper.copy()
-        .then((tempDirectory) => {
-          temp = tempDirectory
+        stubGlobby.withArgs(patterns, options).returns(Promise.resolve(files))
+
+        fileSet.first()
+          .then((file) => {
+            expect(file).to.eql(expected)
+
+            done()
+          })
+          .catch(done)
+      })
+
+      it('should emit a "found" event', (done) => {
+        let expected = [ 'bar.txt' ]
+        let files = expected.concat([ 'baz.md', 'foo.txt', 'fu.md' ])
+        let patterns = [ '**/*.md', '**/*.txt' ]
+        let fileSet = new FileSet(patterns)
+
+        stubGlobby.withArgs(patterns, {}).returns(Promise.resolve(files))
+
+        fileSet.on('found', (files) => {
+          expect(files).to.eql(expected)
 
           done()
         })
-        .catch(done)
+
+        fileSet.first()
+          .catch(done)
+      })
     })
 
-    after(() => {
-      fixtureHelper.cleanUp(temp)
-    })
+    context('when unsuccessful', () => {
+      it('should propagate errors correctly from the "globby" module', (done) => {
+        let expected = new Error('foo')
+        let patterns = [ '**/*.md', '**/*.txt' ]
+        let fileSet = new FileSet(patterns)
 
-    // TODO: Complete unit tests
+        stubGlobby.withArgs(patterns, {}).returns(Promise.reject(expected))
+
+        fileSet.first()
+          .then(() => {
+            expect().fail('Should have been rejected')
+
+            done()
+          })
+          .catch((error) => {
+            expect(error).to.be(expected)
+
+            done()
+          })
+      })
+
+      it('should emit an "error" event', (done) => {
+        let expected = new Error('foo')
+        let patterns = [ '**/*.md', '**/*.txt' ]
+        let fileSet = new FileSet(patterns)
+
+        stubGlobby.withArgs(patterns, {}).returns(Promise.reject(expected))
+
+        fileSet.on('error', (error) => {
+          expect(error).to.eql(expected)
+
+          done()
+        })
+
+        fileSet.first()
+          .then(() => {
+            expect().fail('Should have been rejected')
+
+            done()
+          })
+      })
+    })
   })
 
   describe('#get', () => {
-    let temp
+    context('when successful', () => {
+      it('should use "globby" module and pass the correct glob patterns and options', (done) => {
+        let expected = [ 'bar.txt', 'baz.md', 'foo.txt', 'fu.md' ]
+        let patterns = [ '**/*.md', '**/*.txt' ]
+        let options = { nocase: true, nodir: true }
+        let fileSet = new FileSet(patterns, options)
 
-    before((done) => {
-      fixtureHelper.copy()
-        .then((tempDirectory) => {
-          temp = tempDirectory
+        stubGlobby.withArgs(patterns, options).returns(Promise.resolve(expected))
+
+        fileSet.get()
+          .then((files) => {
+            expect(files).to.eql(expected)
+
+            done()
+          })
+          .catch(done)
+      })
+
+      it('should emit a "found" event', (done) => {
+        let expected = [ 'bar.txt', 'baz.md', 'foo.txt', 'fu.md' ]
+        let patterns = [ '**/*.md', '**/*.txt' ]
+        let fileSet = new FileSet(patterns)
+
+        stubGlobby.withArgs(patterns, {}).returns(Promise.resolve(expected))
+
+        fileSet.on('found', (files) => {
+          expect(files).to.eql(expected)
 
           done()
         })
-        .catch(done)
+
+        fileSet.get()
+          .catch(done)
+      })
     })
 
-    after(() => {
-      fixtureHelper.cleanUp(temp)
-    })
+    context('when unsuccessful', () => {
+      it('should propagate errors correctly from the "globby" module', (done) => {
+        let expected = new Error('foo')
+        let patterns = [ '**/*.md', '**/*.txt' ]
+        let fileSet = new FileSet(patterns)
 
-    // TODO: Complete unit tests
+        stubGlobby.withArgs(patterns, {}).returns(Promise.reject(expected))
+
+        fileSet.get()
+          .then(() => {
+            expect().fail('Should have been rejected')
+
+            done()
+          })
+          .catch((error) => {
+            expect(error).to.be(expected)
+
+            done()
+          })
+      })
+
+      it('should emit an "error" event', (done) => {
+        let expected = new Error('foo')
+        let patterns = [ '**/*.md', '**/*.txt' ]
+        let fileSet = new FileSet(patterns)
+
+        stubGlobby.withArgs(patterns, {}).returns(Promise.reject(expected))
+
+        fileSet.on('error', (error) => {
+          expect(error).to.eql(expected)
+
+          done()
+        })
+
+        fileSet.get()
+          .then(() => {
+            expect().fail('Should have been rejected')
+
+            done()
+          })
+      })
+    })
   })
 
   describe('#last', () => {
-    let temp
+    context('when successful', () => {
+      it('should use "globby" module and pass the correct glob patterns and options', (done) => {
+        let expected = 'fu.md'
+        let files = [ 'bar.txt', 'baz.md', 'foo.txt', expected ]
+        let patterns = [ '**/*.md', '**/*.txt' ]
+        let options = { nocase: true, nodir: true }
+        let fileSet = new FileSet(patterns, options)
 
-    before((done) => {
-      fixtureHelper.copy()
-        .then((tempDirectory) => {
-          temp = tempDirectory
+        stubGlobby.withArgs(patterns, options).returns(Promise.resolve(files))
+
+        fileSet.last()
+          .then((file) => {
+            expect(file).to.eql(expected)
+
+            done()
+          })
+          .catch(done)
+      })
+
+      it('should emit a "found" event', (done) => {
+        let expected = [ 'fu.md' ]
+        let files = [ 'bar.txt', 'baz.md', 'foo.txt' ].concat(expected)
+        let patterns = [ '**/*.md', '**/*.txt' ]
+        let fileSet = new FileSet(patterns)
+
+        stubGlobby.withArgs(patterns, {}).returns(Promise.resolve(files))
+
+        fileSet.on('found', (files) => {
+          expect(files).to.eql(expected)
 
           done()
         })
-        .catch(done)
+
+        fileSet.last()
+          .catch(done)
+      })
     })
 
-    after(() => {
-      fixtureHelper.cleanUp(temp)
+    context('when unsuccessful', () => {
+      it('should propagate errors correctly from the "globby" module', (done) => {
+        let expected = new Error('foo')
+        let patterns = [ '**/*.md', '**/*.txt' ]
+        let fileSet = new FileSet(patterns)
+
+        stubGlobby.withArgs(patterns, {}).returns(Promise.reject(expected))
+
+        fileSet.last()
+          .then(() => {
+            expect().fail('Should have been rejected')
+
+            done()
+          })
+          .catch((error) => {
+            expect(error).to.be(expected)
+
+            done()
+          })
+      })
+
+      it('should emit an "error" event', (done) => {
+        let expected = new Error('foo')
+        let patterns = [ '**/*.md', '**/*.txt' ]
+        let fileSet = new FileSet(patterns)
+
+        stubGlobby.withArgs(patterns, {}).returns(Promise.reject(expected))
+
+        fileSet.on('error', (error) => {
+          expect(error).to.eql(expected)
+
+          done()
+        })
+
+        fileSet.last()
+          .then(() => {
+            expect().fail('Should have been rejected')
+
+            done()
+          })
+      })
+    })
+  })
+
+  describe('#patterns', () => {
+    it('should be read-only', () => {
+      let patterns = [ '**/*.md', '**/*.txt' ]
+
+      let fileSet = new FileSet(patterns)
+      try {
+        fileSet.patterns = [ '**/*.1st' ]
+
+        expect().fail('Should have thrown error')
+      } catch (error) {
+        expect(error).to.be.a(TypeError)
+      }
     })
 
-    // TODO: Complete unit tests
+    it('should not allow modifications', () => {
+      let patterns = [ '**/*.md', '**/*.txt' ]
+
+      let fileSet = new FileSet(patterns)
+      fileSet.patterns.push('**/*.1st')
+
+      expect(fileSet.patterns).to.eql(patterns)
+    })
   })
 })
--- test/moan.spec.js
+++ test/moan.spec.js
@@ -11,14 +11,143 @@
 
 const EventEmitter = require('events').EventEmitter
 const expect = require('expect.js')
+const fs = require('fs')
+const sinon = require('sinon')
 
-const moan = require('../lib/moan')
-const Moan = moan.Moan
+const singleton = require('../lib/moan')
+const Moan = singleton.Moan
 
 describe('moan', () => {
-  // TODO: Complete unit tests
+  it('should be a singleton instance of Moan', () => {
+    expect(singleton).to.be.ok()
+    expect(singleton).to.be.a(Moan)
+  })
 })
 
 describe('Moan', () => {
-  // TODO: Complete unit tests
+  let moan
+  let mockLogger
+
+  beforeEach(() => {
+    moan = new Moan()
+    mockLogger = moan.log = sinon.mock(moan.log)
+  })
+
+  it('should extend from EventEmitter', () => {
+    expect(moan).to.be.an(EventEmitter)
+  })
+
+  describe('#constructor', () => {
+    // TODO: Complete unit tests
+  })
+
+  describe('#currentTask', () => {
+    // TODO: Complete unit tests
+  })
+
+  describe('#dependencies', () => {
+    // TODO: Complete unit tests
+  })
+
+  describe('#failed', () => {
+    // TODO: Complete unit tests
+  })
+
+  describe('#fileSet', () => {
+    // TODO: Complete unit tests
+  })
+
+  describe('#fileSets', () => {
+    it('should be empty when no file sets have been registered', () => {
+      expect(moan.fileSets).to.eql([])
+    })
+
+    it('should be contain all unique registered file sets', () => {
+      moan.fileSet('foo', [])
+      moan.fileSet('bar', [])
+      moan.fileSet('foo', [])
+
+      expect(moan.fileSets).to.eql([ 'foo', 'bar' ])
+    })
+
+    it('should be read-only', () => {
+      try {
+        moan.fileSets = [ 'foo' ]
+
+        expect().fail('Should have thrown error')
+      } catch (error) {
+        expect(error).to.be.a(TypeError)
+      }
+    })
+
+    it('should not allow modifications', () => {
+      moan.fileSets.push('foo')
+
+      expect(moan.fileSets).to.eql([])
+    })
+  })
+
+  describe('#finished', () => {
+    // TODO: Complete unit tests
+  })
+
+  describe('#result', () => {
+    // TODO: Complete unit tests
+  })
+
+  describe('#run', () => {
+    // TODO: Complete unit tests
+  })
+
+  describe('#started', () => {
+    // TODO: Complete unit tests
+  })
+
+  describe('#task', () => {
+    // TODO: Complete unit tests
+  })
+
+  describe('#tasks', () => {
+    it('should be empty when no tasks have been registered', () => {
+      expect(moan.tasks).to.eql([])
+    })
+
+    it('should be contain all unique registered tasks', () => {
+      moan.task('foo')
+      moan.task('bar')
+      moan.task('foo')
+
+      expect(moan.tasks).to.eql([ 'foo', 'bar' ])
+    })
+
+    it('should be read-only', () => {
+      try {
+        moan.tasks = [ 'foo' ]
+
+        expect().fail('Should have thrown error')
+      } catch (error) {
+        expect(error).to.be.a(TypeError)
+      }
+    })
+
+    it('should not allow modifications', () => {
+      moan.tasks.push('foo')
+
+      expect(moan.tasks).to.eql([])
+    })
+  })
+
+  describe('#version', () => {
+    it('should match the version in the package descriptor', (done) => {
+      fs.readFile('package.json', 'utf8', (error, data) => {
+        if (error) {
+          done(error)
+        } else {
+          expect(moan.version).to.be(JSON.parse(data).version)
+
+          done()
+        }
+      })
+    })
+  })
 })
